13d575cdd49b1d10118d2030cff1eeff7fa8ab4b
Wolfgang Geisler <pw18l004@technikum-wien.at>
2021-06-22 12:42:36 +0200

    init
    Co-authored-by: Daniel <danielmuxel@users.noreply.github.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,99 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="products" class="products">
+      <h1>Unsere Produkte</h1>
+      <div v-for="product in products" :key="product.id" class="product">
+        <div class="product-header">
+          {{ product.name }} - {{ product.description }}
+        </div>
+        <div class="product-footer">{{ product.price }}</div>
+        <div class="actions">
+          <button @click="deleteProduct(product.id)">Delete</button>
+        </div>
+      </div>
+    </div>
+    
+    <div class="product-form">
+      {{ name }} - {{ description }} - {{ price }}
+      <form @submit.prevent="createProduct">
+        <div class="form-control">
+          <label>Name</label>
+          <input v-model="name" type="text" />
+        </div>
+        <div class="form-control">
+          <label>Beschreibung</label>
+          <input v-model="description" type="text" />
+        </div>
+        <div class="form-control">
+          <label>Preis</label>
+          <input v-model="price" type="number" />
+        </div>
+        <div class="form-control">
+          <button type="submit">Hinzufügen</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// axios importieren
+import axios from "axios";
+const url = "http://localhost:3000/products";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      products: "",
+      name: "",
+      description: "",
+      price: null,
+    };
+  },
+  mounted() {
+    // getProducts aufrufen
+    this.getProducts();
+  },
+  methods: {
+    getProducts() {
+      axios.get(url).then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+    createProduct() {
+      axios
+        .post(url, {
+          name: this.name,
+          description: this.description,
+          price: this.price,
+        })
+        .then((response) => {
+          this.products.push(response.data);
+        });
+    },
+    deleteProduct(id) {
+      axios
+        .delete(url, id, {
+          id: this.id,
+          name: this.name,
+          description: this.description,
+          price: this.price,
+        });
+    }
+  },
+};
 </script>
+
+<style>
+.product {
+  margin-bottom: 2rem;
+}
+
+.form-control {
+  margin-bottom: 1rem;
+}
+</style>
